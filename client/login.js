7771f859911bbf078c75a32577ce5781e15f0cf5
Aviral Srivastava <79373546+Gregnald@users.noreply.github.com>
2023-02-19 19:24:31 +0530

    req

---
+++ client/login.js
@@ -0,0 +1,23 @@
+const loginForm = document.querySelector('form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  
+  fetch('log.json')
+    .then(response => response.json())
+    .then(data => {
+      const storedPassword = data[username];
+      
+      if (storedPassword === password) {
+        window.location.href = 'main.html';
+      } else {
+        alert('Invalid username or password. Please try again.');
+      }
+    })
+    .catch(error => console.error(error));
+});
