d7da91666fe2dcd7e49d0f7df85fd8b2f62f7022
Aviral Srivastava <79373546+Gregnald@users.noreply.github.com>
2023-02-21 00:45:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,13 +21,7 @@ app.get('/', async (req, res) => {
   })
 })
 let prompt ="Scriptor is a Animation Script writer bot.\n"
-//prompt+="Scriptor is an Animation Script writing bot that writes interesting scripts.\n"
-//prompt+="user : hi\nScriptor : Sup? nigga!\n"
-/*prompt+="user : write a 5 second animation script based on boats in ocean.\nScriptor : \n"
-prompt+="Title: Bobbing Boats\n\n~Fade in~\n\nThe camera shows a wide shot of the ocean with several boats bobbing up and down in the waves.\n\n~Cut to~\n\nA close-up of a single boat, with its sails flapping in the wind.\n\n~Cut to~\n\nAnother boat appears in the background, moving towards the camera.\n\n~Cut to~\n\nThe two boats pass each other, their wakes creating ripples in the water.\n\n~Cut to~\n\nThe camera shows a wide shot of the ocean again, with the boats bobbing up and down as they sail away.\n\n~Fade out~\n"
-prompt+="user : write a 1 min animation of a man and coconut tree.\nScriptor : \n"
-prompt+="Title: The Man and the Coconut Tree\n\n~Fade in~\n\nEXT. A deserted island - DAY\n\nWe see a man stranded on a deserted island, looking for food and water.\n\n~CUT TO~\n\nThe man walks around the island and finds a coconut tree.\n\n~CUT TO~\n\nThe man climbs the tree to get some coconuts.\n\n~CUT TO~\n\nAs he climbs down, he accidentally drops one of the coconuts, hitting him on the head and knocking him out.\n\n~CUT TO~\n\nThe man wakes up and sees the coconut tree bending down towards him, as if it's offering him a coconut.\n\n~CUT TO~\n\nThe man takes the coconut and drinks the refreshing water inside.\n\n~CUT TO~\n\nThe man continues to drink from the coconut, feeling rejuvenated and grateful for the tree's gift.\n\n~CUT TO~\n\nThe man looks up at the tree and smiles, realizing that he's not alone on the island after all.\n\n~Fade out~"
-*/app.post('/', async (req, res) => {
+app.post('/', async (req, res) => {
   try {
     prompt += `user : ${req.body.prompt}Scriptor : `;
     //prompt = `${req.body.prompt}`;
